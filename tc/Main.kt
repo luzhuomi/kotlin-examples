15cf91e639d91fa3848b0c18762ad28c75e4067f
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2022-11-20 22:42:12 +0800

    updated

---
+++ tc/Main.kt
@@ -0,0 +1,29 @@
+
+
+fun main() {
+    println("Hello, world!!!")
+    ShowObj.showPoint().run{ // built-in run method makes the object extension method in-scope
+        val p = Point(100.0f,10.0f)
+        println(p.show())
+    }
+}
+
+// type class
+interface Show<T> {
+    fun T.show() : String
+}
+
+class Point(val x:Float, val y:Float) {
+    
+}
+
+// instance objects
+object ShowObj {
+    fun showPoint() : Show<Point> = ShowPoint 
+
+    object ShowPoint : Show<Point> {
+        override fun Point.show() : String {
+            return this.x.toString() + " " + this.y.toString()
+        }
+    }
+}
